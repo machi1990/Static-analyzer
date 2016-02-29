cbbbe329841507835a380a497cc9ed65d6ea0e46
Manyanda Chitimbo <manyanda.chitimbo@safety-line.fr>
2016-02-29 11:48:42 +0100

    Initial commit

---
+++ tests/observation/loop.c
@@ -0,0 +1,7 @@
+{
+    int x;
+    int y;
+    y = 6;
+    x = 23;
+    assert(x < y);
+}
---
+++ tests/observation/rand.c
@@ -0,0 +1,9 @@
+{
+    int x;
+    int y;
+    
+    x = rand(-1,10);
+    
+    assert(x < 1);
+    print(x);
+}
