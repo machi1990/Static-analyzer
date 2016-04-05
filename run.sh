18ae21a41199c339d2bfbbfc819134d831d96b30
Manyanda Chitimbo <manyanda.chitimbo@safety-line.fr>
2016-04-05 19:04:15 +0200

    scripts reviewing

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 IFS='
 '
-
+sf_present=false
+rf_present=false
 
 # read the options"
 delay=0
@@ -9,7 +10,8 @@ unroll=3
 narrow=0
 
 # extract options and their arguments into variables.
-while getopts ":d:,:u:,:n:,h" opt; do
+options=":d:,:u:,:n:,h,:s:,:r:"
+while getopts $options opt; do
     case $opt in
     d)
         if [ "$OPTARG" -gt "$delay" ]
@@ -32,18 +34,33 @@ while getopts ":d:,:u:,:n:,h" opt; do
         fi
       ;;
 
+    s)
+        sf_present=true
+        SOURCE=$OPTARG
+        ;;
+
+    r)
+        rf_present=true
+        RESULT=$OPTARG
+        ;;
     h)
         echo "Usage: [options] [argument] ..."
         echo "Options: "
+        echo "-s Name                 Folder's name where source files are located. Required option."
+        echo "-r Name                 Result's folder name. Required option."
         echo "-d N                    N iterations for loop's widening delay."
         echo "-u N                    N iterations for loop unrolling."
         echo "-n N                    N iterations for loop's fixed point narrowing."
         echo "-h                      Print this message."
         echo " "
-        echo "This program built for Unix's OS."
+        echo "This script targets Unix's OS."
         echo "Report bugs to Manyanda Chitimbo <chitson1990@gmail.com>."
         exit 1
         ;;
+
+    *  ) echo "Unimplemented option: -$OPTARG" >&2;
+    exit 1;;
+
     \?)
       echo "Invalid option: -$OPTARG" >&2
       exit 1
@@ -64,6 +81,71 @@ while getopts ":d:,:u:,:n:,h" opt; do
   esac
 done
 
+#check for presence of required options
+if ! $rf_present
+then
+    echo "Result directory must be included when a directory is specified" >&2
+    exit 1
+fi
+
+RESULT=$RESULT"/"
+RESULT=$(echo $RESULT | perl -pe "s/(\/){2,}/\//g")
+
+if ! [ -d "$RESULT" ]; then
+    # result folder creation
+    mkdir -p $RESULT
+    if [ $? -ne 0 ] ; then
+        echo "Error: Permission denied. "
+        exit 1;
+    else
+        echo "Result folder created successfully.";
+    fi
+fi
+
+interval=$RESULT"interval/"
+constant=$RESULT"constant/"
+reduced=$RESULT"reduced/"
+parity=$RESULT"parity/"
+partition_interval=$RESULT"partition-interval/"
+partition_constant=$RESULT"partition-constant/"
+
+
+# Create a folder for each analaysis
+mkdir -p $interval $constant $reduced $parity \
+$partition_interval $partition_constant
+
+if [ $? -ne 0 ] ; then
+    echo "Error: Permission denied. "
+    exit 1;
+fi
+
+
+if ! $sf_present
+then
+    echo "Source directory must be included when a directory is specified" >&2
+    exit 1
+else
+    if ! [ -d "$SOURCE" ]; then
+        echo $SOURCE" does not exists." >&2
+        exit 1
+    fi
+
+    SOURCE=$SOURCE"/"
+    SOURCE=$(echo $SOURCE | perl -pe "s/(\/){2,}/\//g")
+
+    LENGTH=$((${#SOURCE}-1))
+    SOURCE_1=$(echo ${SOURCE:0:LENGTH})
+
+    for file in $(find $SOURCE_1 -type d | cut -sd / -f 2-)
+    do
+        sub_directory="/"$file
+        #creates subdirectory
+        mkdir -p $interval$sub_directory $constant$sub_directory \
+               $reduced$sub_directory  $parity$sub_directory \
+              $partition_interval$sub_directory $partition_constant$sub_directory
+    done
+fi
+
 # make clean and make
 echo "STARTS MAKE CLEAN"
 make clean
@@ -71,60 +153,41 @@ make clean
 echo "MAKE CLEAN FINISHED. STARTS MAKE."
 make
 
-echo "MAKE FINISHED. STARTS EXECUTION."
-
-# result folder creation
-result="result/"
-rm -rf $result && mkdir $result
-interval=$result"interval/"
-constant=$result"constant/"
-reduced=$result"reduced/"
-parity=$result"parity/"
-partition_interval=$result"partition-interval/"
-partition_constant=$result"partition-constant/"
-
-mkdir $interval $constant $reduced $parity $partition_interval $partition_constant
-
-observation="observation"
-mkdir $interval$observation $constant$observation \
-       $reduced$observation  $parity$observation \
-      $partition_interval$observation $partition_constant$observation
+echo "MAKE FINISHED. STARTS ANALYSIS."
 
 set -f
-DIR="tests/"
-TXT=".txt"
-
+extension=".txt"
 # analysis of all tests files
 
-for file in $(find "tests" -name *.c | cut -sd / -f 2-)
+for file in $(find $SOURCE_1 -name *.c | cut -sd / -f 2-)
 do
-    filename=$interval$file$TXT
+    filename=$interval$file$extension
 
-    echo "Starts analysis of: "$DIR$file
-    ./analyzer.byte -interval $DIR$file -unroll $unroll -delay $delay \
+    echo "Starts analysis of: "$SOURCE$file
+    ./analyzer.byte -interval $SOURCE$file -unroll $unroll -delay $delay \
     -narrow $narrow > $filename
 
-    filename=$constant$file$TXT
-    ./analyzer.byte -constant $DIR$file -unroll $unroll -delay $delay \
+    filename=$constant$file$extension
+    ./analyzer.byte -constant $SOURCE$file -unroll $unroll -delay $delay \
     -narrow $narrow > $filename
 
-    filename=$reduced$file$TXT
-    ./analyzer.byte -reduced $DIR$file -unroll $unroll -delay $delay \
+    filename=$reduced$file$extension
+    ./analyzer.byte -reduced $SOURCE$file -unroll $unroll -delay $delay \
     -narrow $narrow > $filename
 
-    filename=$parity$file$TXT
-    ./analyzer.byte -parity $DIR$file -unroll $unroll -delay $delay \
+    filename=$parity$file$extension
+    ./analyzer.byte -parity $SOURCE$file -unroll $unroll -delay $delay \
     -narrow $narrow > $filename
 
-    filename=$partition_interval$file$TXT
-    ./analyzer.byte -partition-interval $DIR$file -unroll $unroll -delay $delay \
+    filename=$partition_interval$file$extension
+    ./analyzer.byte -partition-interval $SOURCE$file -unroll $unroll -delay $delay \
     -narrow $narrow > $filename
 
-    filename=$partition_constant$file$TXT
-    ./analyzer.byte -partition-constant $DIR$file -unroll $unroll -delay $delay \
+    filename=$partition_constant$file$extension
+    ./analyzer.byte -partition-constant $SOURCE$file -unroll $unroll -delay $delay \
     -narrow $narrow > $filename
 
-    echo "Analysis of: "$DIR$file" finished."
+    echo "Analysis of: "$SOURCE$file" finished."
     echo " "
 done
 
