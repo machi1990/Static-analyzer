e46e575dfb03876f67505d8df2ec01ff4c899e96
Manyanda Chitimbo <manyanda.chitimbo@safety-line.fr>
2016-04-13 19:12:55 +0200

    Small modif

--- tests/observation/division.c
+++ tests/observation/division.c
@@ -1,12 +1,18 @@
 {
     int x;
     int y;
+    int z;
     
     y = 1;
-    x = 1;
+    x = rand(-1,4);
     
-    if ( 1 == 1) {
-        
+    if ( y == x) {
+        z = rand(10,19)/0;
+        print(z);
+    } else {
+        z = x /rand (-1,2);
+        print(z);
     }
     
+    print(z);
 }
